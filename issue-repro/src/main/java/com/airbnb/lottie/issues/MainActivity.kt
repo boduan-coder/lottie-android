340a1aaeaa1407ffdb58ee6663c792ce417df18f
Bo Duan <boduan@google.com>
2019-12-03 16:48:13 -0800

    Test url loading while in airplane mode

--- issue-repro/src/main/java/com/airbnb/lottie/issues/MainActivity.kt
+++ issue-repro/src/main/java/com/airbnb/lottie/issues/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityOptionsCompat
+import com.airbnb.lottie.LottieAnimationView
 import com.airbnb.lottie.issues.R
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -12,5 +13,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         // Reproduce any issues here.
+        val lottieAnimationView: LottieAnimationView = findViewById(R.id.lottie_v)
+        lottieAnimationView.setAnimationFromUrl("https://assets5.lottiefiles.com/packages/lf20_hfgbno.json")
     }
 }
